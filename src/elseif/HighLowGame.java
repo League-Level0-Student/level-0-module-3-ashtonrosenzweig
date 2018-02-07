46cc569a62ea45082fd13db0e1215b4b0d210e27
Rosenzweig.Ashton <Rosenzweig.Ashton@SEMS-259-32.SEMS259.LAB>
2018-02-07 14:50:25 -0800

    HighLow, ColorChooser

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,29 +5,43 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int tryagain = 0;
+		// 3. Change this line to give you a random number between 1 - 100.
+		int random = new Random().nextInt(101 + 1);
 		// 2. Print out the random variable above
-		
+		System.out.println(random);
 		// 11. do the following 10 times
-		
-			// 1. ask the user for a guess using a pop-up window, and save their response 
+		while (tryagain == 0) {
 
+			// 1. ask the user for a guess using a pop-up window, and save their response
+			String guess = JOptionPane
+					.showInputDialog("I am thinking of a number 1-100, what number am I thinking of?");
 			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-			
+			int guessn = Integer.parseInt(guess);
 			// 5. if the guess is correct
-				// 6. win
+			if (guessn == random) {
+			tryagain = JOptionPane.showConfirmDialog(null, "You win! Play again?");
+			}
+			// 6. win
 			// 7. if the guess is high
-				// 8. tell them it's too high
+			if (guessn > random) {
+				tryagain = JOptionPane.showConfirmDialog(null, "Your guess is too high! Try again?");
+
+			}
+			// 8. tell them it's too high
 			// 9. if the guess is low
-				// 10. tell them it's too low
+			// 10. tell them it's too low
+			if (guessn < random) {
+				tryagain = JOptionPane.showConfirmDialog(null, "Your guess is too low! Try again?");
+				// 12. tell them they lose
 
-		// 12. tell them they lose
+			}
+
+		}
 	}
-
 }
-
-
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -5,23 +5,69 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
+import java.util.Random;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
+		Robot rob = new Robot();
+		int whilee = 0;
+		while (whilee == 0) {
+			rob.clear();
+			Random rand = new Random();
+			int random1 = rand.nextInt(300) + 1;
+			int random2 = rand.nextInt(300) + 1;
+			int random3 = rand.nextInt(300) + 1;
+			System.out.println("Random: " + random1 + ", " + random2 + ", " + random3);
+			// 3. ask the user what color they would like the robot to draw
+			rob.moveTo(450, 300);
+			int color = JOptionPane.showOptionDialog(null, "What color do you want?", "Pop-up Title", 0,
+					JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Red", "Green", "Blue", "Orange", "Yellow", "Purple", "Random" }, null);
+			// 4. use an if/else statement to set the pen color that the user requested
+			if (color == 0) {
+				rob.setPenColor(255, 0, 0);
+			}
+			if (color == 1) {
+				rob.setPenColor(0, 255, 0);
+			}
+			if (color == 2) {
+				rob.setPenColor(0, 0, 255);
+			}
+			if (color == 3) {
+				rob.setPenColor(255, 165, 0);
+			}
+			if (color == 4) {
+				rob.setPenColor(255, 255, 0);
+			}
+			if (color == 5) {
+				rob.setPenColor(128, 0, 128);
+			}
+			if (color == 6) {
+				rob.setPenColor(random1, random2, random3);
+			}
+			// 5. if the user doesn’t enter anything, choose a random color
 
-		//3. ask the user what color they would like the robot to draw
-		
-		//4. use an if/else statement to set the pen color that the user requested
-
-        //5. if the user doesn’t enter anything, choose a random color
-
-        //6. put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//2. set the pen width to 10
-		
-	    //1. make the robot draw a shape (this will take more than one line of code)
-
-
+			// 2. set the pen width to 10
+			rob.setPenWidth(10);
+			rob.penDown();
+			// 1. make the robot draw a shape (this will take more than one line of code)
+			rob.setSpeed(25);
+			for (int i = 0; i < 3; i++) {
+				rob.move(75);
+				rob.turn(-90);
+			}
+			rob.move(75);
+			rob.penUp();
+			rob.move(200);
+			rob.turn(-90);
+			whilee = JOptionPane.showConfirmDialog(null, "Do you want to keep playing?");
+			if (whilee == 1) {
+				System.exit(0);
+			}
+		}
 	}
 }
